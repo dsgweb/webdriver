903f59c81b53f39c3f50b6463608fdac7721160a
oskey <dsgweb@gmail.com>
2013-02-06 10:31:23 -0200

    message

---
+++ TestProject/src/com/example/tests/library/TestCloudServices.java
@@ -0,0 +1,22 @@
+package com.example.tests.library;
+
+/*
+import org.testng.Assert;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import com.template.pageobjects.CloudServicesPage;
+import com.template.pageobjects.HomePage;
+import com.template.util.ParentTestCase;
+*/
+
+/**
+ * 
+ * @author Altimetrik
+ *
+ */
+public class TestCloudServices  {
+
+	//private HomePage homePage;
+	
+}
